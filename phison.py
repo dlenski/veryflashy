b06989ce6e4d6649c37c9db00c9de5797745a87a
Daniel Lenski <dlenski@gmail.com>
2026-05-10 01:29:52 -0700

    Off-by-one in number of blocks
    The value returned by SCSI read-cap is the index of the last block, thus need +1

--- phison.py
+++ phison.py
@@ -50,7 +50,8 @@ print(f'  Phison USB ID {usb_vid:04x}:{usb_pid:04x}')
 
 print("Reading standard SCSI disk capacity (SCSI command 25 ...):")
 res = sgread(fd, bytes.fromhex('25 00 00 00 00 00 00 00 00 00 00 00 00 00'), 8)
-nblks, blksize = int.from_bytes(res[:4], 'big'), int.from_bytes(res[4:8], 'big')
+nblks = int.from_bytes(res[:4], 'big') + 1
+blksize = int.from_bytes(res[4:8], 'big')
 print(f'  SCSI block size {blksize} x {nblks} = {humanize.naturalsize(blksize*nblks)}')
 
 # 'INFO' read
